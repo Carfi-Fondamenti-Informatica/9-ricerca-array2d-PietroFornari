e84a8219cdb83e0c53907f8890d9f5b11cfcc6bf
PietroFornari <100316910+PietroFornari@users.noreply.github.com>
2022-05-15 20:26:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+    char lista[10][20];
+    char cerca [20];
+    init (lista);
+    init (cerca);
+    for (int i=0; i<10; i++){
+        cin >> lista [i];}
+    cin >> cerca ;
+            int a = ricerca (lista, cerca);
+            if (a!=-1) {
+                cout << a;
+            } else {cout << "non presente" << endl;}
   return 0;
 }
